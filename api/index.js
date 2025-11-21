8b12cb56fdee79fe57ddc89092af5d8abbbd4777
Amr3011 <amroosamafakharany@gmail.com>
2025-11-21 13:24:45 +0200

    Fix Vercel routing configuration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
 // Vercel serverless function entry point
-// Import and initialize the app
 const app = require("../src/server");
 
-// Export as a serverless function handler
-module.exports = async (req, res) => {
-  return app(req, res);
-};
+// Export the Express app directly for Vercel
+module.exports = app;
